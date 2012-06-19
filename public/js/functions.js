e238828de578f3f44702bfe2531125d2284561cc
Devon St. Clair <devonstclair@gmail.com>
2012-06-19 14:40:01 -0700

    Front end code nearing completion

--- public/js/functions.js
+++ public/js/functions.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
   //Global vars
   var _currentImage;
   var _currentCircle;
+  var _currentElement;
+  var _currentPage = '#first_showcase '
   var _lastImage;
   var _elements           = $('.detail-slide.active .media-container li');
   var _indicator_elements = $('.detail-slide.active .place_indicator li');
@@ -14,7 +16,7 @@ $(document).ready(function() {
 
   function processSlide() {
     $('.media-container li').live('click', showNextImage);
-    //$('#first_showcase .place_indicator li').bind('click', triggerNextImage);
+    $('.place_indicator li').live('click', triggerNextImage);
   }
 
   function processPage() {
@@ -22,9 +24,10 @@ $(document).ready(function() {
     $('#second-process').live('click', showPreviousProcess);
   }   
 
-  function showNextProcess(e) {
+  function showNextProcess(event) {
     
     event.preventDefault();
+    
     _currentProcess = $('#first_showcase').parent();
     _nextProcess    = $('#second_showcase').parent();
     $(_currentProcess[0]).fadeOut('slow', function() {
@@ -32,12 +35,13 @@ $(document).ready(function() {
       $(_nextProcess[0]).fadeIn();
       $(_nextProcess[0]).addClass('active');
       $(_currentProcess[0]).removeClass('active');
+      _currentPage = '#second_showcase ';
     });
     _elements = $('#second_showcase .media-container li');
     _indicator_elements = $('#second_showcase .place_indicator li');
   }
 
-  function showPreviousProcess(e) {
+  function showPreviousProcess(event) {
     
     event.preventDefault();
     _currentProcess = $('#second_showcase').parent();
@@ -47,54 +51,99 @@ $(document).ready(function() {
       $(_nextProcess[0]).fadeIn();
       $(_nextProcess[0]).addClass('active');
       $(_currentProcess[0]).removeClass('active');
+      _currentPage = '#first_showcase '
     });
 
     _elements = $('#first_showcase .media-container li');
     _indicator_elements = $('#first_showcase .place_indicator li');
   }
 
-  function triggerNextImage(e) {
+  function triggerNextImage(event) {
     event.preventDefault();
+
+    $(event.target).siblings().removeClass('active');
+
+    _currentElement = '.' + $(event.target).attr('class').split(' ')[0];
+
+    _selectedElements = _currentPage + _currentElement;
     
-    var bleh = $(e.target).attr('class')
-    //var test = $('#first_showcase .media-container', bleh);
-    console.log(bleh)
-    
-    var blahzors = $('#first_showcase .media-container,' + '.' + bleh)
-    $('#first_showcase .media-container li').removeClass('active')
-    $(blahzors).addClass('active')
-    //$(blahzors).removeClass('active')
-    console.log('crazy>>', $('#first_showcase .media-container .second'))
-    
-    //$(blahzors).trigger('click');
+    $(_selectedElements).siblings().removeClass('active');
+    $(_selectedElements).addClass('active');
   }
 
-  function showNextImage(e) {
-    e.preventDefault();
+  function showNextImage(event) {
+    event.preventDefault();
 
     _lastImage     = $(_elements[2]);
-    _currentImage = e.target;
-    _currentCircle = $(_currentImage).parent().siblings().find('.active')
+    _currentImage = event.target;
    
     if($(_currentImage).hasClass('active')) {
     
       if(_currentImage != _lastImage[0]) {
-        $(this).removeClass('active');
-        $(this).next().addClass('active');
 
-        $(_currentCircle[0]).removeClass('active');
-        $(_currentCircle[0]).next().addClass('active');
+        _currentElement = '.' + $(event.target).next().attr('class').split(' ')[0];
+        _selectedElements = _currentPage + _currentElement;
+
+        $(_selectedElements).siblings().removeClass('active');
+        $(_selectedElements).addClass('active');
       
       }
       else {
-        $(_currentImage).removeClass('active');
-        $(_currentCircle[0]).removeClass('active');
 
-        _currentImage = $(_elements[0]);
-        _currentCircle = $(_indicator_elements[0])
-        $(_currentImage).addClass('active');
-        $(_currentCircle).addClass('active');
+        _currentElement = '.' + $(_elements[0]).attr('class');
+        _selectedElements = _currentPage + _currentElement;
+
+        $(_selectedElements).siblings().removeClass('active');
+        $(_selectedElements).addClass('active');
       }
     }
+  }
+
+  //  Anchor Scrolling, a la the fancy CSS Tricks
+  //  http://css-tricks.com/snippets/jquery/smooth-scrolling/
+  function filterPath(string) {
+    return string
+    .replace(/^\//,'')
+    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+    .replace(/\/$/,'');
+  }
+  var locationPath = filterPath(location.pathname);
+  var scrollElem = scrollableElement('html', 'body');
+ 
+  $('a[href*=#]').each(function() {
+    var thisPath = filterPath(this.pathname) || locationPath;
+    if (  locationPath == thisPath
+    && (location.hostname == this.hostname || !this.hostname)
+    && this.hash.replace(/#/,'') ) {
+      var $target = $(this.hash), target = this.hash;
+      if (target) {
+        var targetOffset = $target.offset().top;
+        $(this).click(function(event) {
+          event.preventDefault();
+          $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
+            location.hash = target;
+          });
+        });
+      }
+    }
+  });
+ 
+  // use the first element that is "scrollable"
+  function scrollableElement(els) {
+    for (var i = 0, argLength = arguments.length; i <argLength; i++) {
+      var el = arguments[i],
+          $scrollElement = $(el);
+      if ($scrollElement.scrollTop()> 0) {
+        return el;
+      } else {
+        $scrollElement.scrollTop(1);
+        var isScrollable = $scrollElement.scrollTop()> 0;
+        $scrollElement.scrollTop(0);
+        if (isScrollable) {
+          return el;
+        }
+      }
+    }
+    return [];
   }
 });
